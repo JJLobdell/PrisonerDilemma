7ed0b808ee220d7e705f9966212a6b9237775e77
Logan <RochaBros25@live.com>
2017-11-15 08:22:49 -0600

    Lock & Load
    Shake and bake!

--- team0.py
+++ team0.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Luke and Logan' # Only 10 chars displayed.
+strategy_name = 'Lock & Load'
+strategy_description = 'snitches get stitches'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
